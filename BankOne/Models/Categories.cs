566d40b8be3c1fa71bc55eedeccdbaa45a09d992
Pedro Couto <2191265@my.ipleiria.pt>
2021-12-02 17:10:50 +0000

    Created Models. Updated Tables

---
+++ BankOne/Models/Categories.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankOne.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public char Type { get; set; }
+        public int User_id { get; set; }
+    }
+}
---
+++ BankOne/Models/VCardTransactions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankOne.Models
+{
+    public class VCardTransactions
+    {
+        public int Id { get; set; }
+        public string VCard { get; set; }
+        public DateTime Date { get; set; }
+        public char Type { get; set; }
+        public decimal Value { get; set; }
+        public int Category_id { get; set; }
+        public string Description { get; set; }
+    }
+}
