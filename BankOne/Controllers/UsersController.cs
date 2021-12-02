bc0439bcacb5bf3ff4d9757ccb78338496a84348
Pedro Couto <2191265@my.ipleiria.pt>
2021-12-02 19:44:15 +0000

    UserController quase feito

--- BankOne/Controllers/UsersController.cs
+++ BankOne/Controllers/UsersController.cs
@@ -5,7 +5,10 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Web.Http;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace BankOne.Controllers
 {
@@ -14,7 +17,7 @@ namespace BankOne.Controllers
         string connectionString = Properties.Settings.Default.BankOneDBConnection;
 
 
-        public IEnumerable<User> GetAllUsers()
+        public IHttpActionResult GetAllUsers()
         {
             List<User> users = new List<User>();
 
@@ -30,7 +33,6 @@ namespace BankOne.Controllers
 
                 while (reader.Read())
                 {
-                    // Converter da tabela para objeto Product
                     User user = new User
                     {
                         Id = (int)reader["Id"],
@@ -42,21 +44,236 @@ namespace BankOne.Controllers
                     };
                     users.Add(user);
                 }
+
                 reader.Close();
 
                 conn.Close();
             }
             catch (Exception)
             {
-
                 if (conn.State == System.Data.ConnectionState.Open)
                 {
                     conn.Close();
                 }
+                return InternalServerError();
+
 
             }
-            return users;
+
+            return Ok(users);
 
         }
+
+        public IHttpActionResult GetById(int id)
+        {
+            List<User> users = new List<User>();
+
+            SqlConnection conn = null;
+
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Users WHERE id = @id", conn);
+                command.Parameters.AddWithValue("@id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                User user = null;
+
+                if (reader.Read())
+                {
+                    user = new User
+                    {
+                        Id = (int)reader["Id"],
+                        Name = (string)reader["name"],
+                        Email = (string)reader["email"],
+                        Photo_url = reader["photo_url"] == DBNull.Value ? "" : (string)reader["photo_url"],
+                        Phone_number = (string)reader["phone_number"]
+                    };
+                }
+
+                reader.Close();
+
+                conn.Close();
+
+                if (user != null)
+                {
+                    return Ok(user);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+
+                    conn.Close();
+
+                }
+                return BadRequest();
+
+            }
+
+        }
+        public IHttpActionResult PostUser([FromBody] User user)
+        {
+            SqlConnection connection = null;
+
+            try
+            {
+
+                connection = new SqlConnection(connectionString);
+                string sql = "INSERT INTO Users VALUES (@name, @email, @password, @photo_url, @confirmation_code, @phone_number)";
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                command.Parameters.AddWithValue("@name", user.Name);
+                command.Parameters.AddWithValue("@email", user.Email);
+                using (SHA256 mySHA256 = SHA256.Create())
+                {
+                    command.Parameters.AddWithValue("@password", Convert.ToBase64String(mySHA256.ComputeHash(Encoding.UTF8.GetBytes(user.Password))));
+                    command.Parameters.AddWithValue("@confirmation_code", Convert.ToBase64String(mySHA256.ComputeHash(Encoding.UTF8.GetBytes(user.Confirmation_code))));
+
+                }
+
+                command.Parameters.AddWithValue("@photo_url", user.Photo_url ?? "");
+                command.Parameters.AddWithValue("@phone_number", user.Phone_number);
+
+
+                int numeroRegistos = command.ExecuteNonQuery();
+
+                connection.Close();
+
+                if (numeroRegistos > 0)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+
+                    connection.Close();
+                }
+                return InternalServerError();
+            }
+        }
+
+        public IHttpActionResult PutUser(int id, [FromBody] User user)
+        {
+
+            SqlConnection connection = null;
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+
+
+                string sql = "UPDATE Users SET Name = @name, Email = @email, Photo_url = @photo_url, Phone_number = @phone_number WHERE id = @id";
+
+                connection.Open();
+                SqlCommand commandSearch = new SqlCommand("SELECT * FROM Users WHERE id = @id", connection);
+                commandSearch.Parameters.AddWithValue("@id", id);
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader reader = commandSearch.ExecuteReader();
+
+                if (!reader.Read())
+                {
+                    reader.Close();
+                    connection.Close();
+                    return NotFound();
+                }
+
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@name", user.Name ?? (string)reader["name"]);
+                command.Parameters.AddWithValue("@email", user.Email ?? (string)reader["email"]);
+                command.Parameters.AddWithValue("@photo_url", user.Photo_url ?? (string)reader["photo_url"]);
+                command.Parameters.AddWithValue("@phone_number", user.Phone_number ?? (string)reader["phone_number"]);
+
+                reader.Close();
+
+                int numeroRegistos = command.ExecuteNonQuery();
+
+                connection.Close();
+
+                if (numeroRegistos > 0)
+                {
+                    return Ok(user);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+
+                    connection.Close();
+                }
+
+                return InternalServerError(e);
+            }
+
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+            SqlConnection connection = null;
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+
+                string sql = "DELETE FROM Users WHERE Id = @id";
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                int numeroRegistos = command.ExecuteNonQuery();
+
+                connection.Close();
+
+                if (numeroRegistos > 0)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+
+                    connection.Close();
+                }
+                return InternalServerError();
+            }
+        }
     }
+
 }
--- BankOne/Models/User.cs
+++ BankOne/Models/User.cs
@@ -9,6 +9,7 @@ namespace BankOne.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public string Photo_url { get; set; }
         public string Confirmation_code { get; set; }
