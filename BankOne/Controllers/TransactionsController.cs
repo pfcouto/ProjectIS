31801e351716467c16ebbe57479b4a13d542b2c7
Pedro Couto <2191265@my.ipleiria.pt>
2021-12-09 23:49:49 +0000

    TransactionController

--- BankOne/Controllers/TransactionsController.cs
+++ BankOne/Controllers/TransactionsController.cs
@@ -15,6 +15,83 @@ namespace BankOne.Controllers
     {
         readonly string connectionString = Properties.Settings.Default.BankOneDBConnection;
 
+        [Route("api/transactions")]
+        public IHttpActionResult GetTransactions(string type = null, string dateFrom = null, string dateTo = null)
+        {
+            List<VCardTransaction> transactions = new List<VCardTransaction>();
+
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+
+
+                string sqlString = "SELECT * FROM VCardTransactions";
+
+                SqlCommand command = new SqlCommand(null, conn);
+
+
+                if (type != null)
+                {
+                    sqlString += " WHERE type = @type";
+                    command.Parameters.AddWithValue("@type", type);
+                }
+
+                if (dateFrom != null && dateTo != null)
+                {
+                    if (!sqlString.Contains("WHERE"))
+                    {
+                        sqlString += " WHERE ";
+                    }
+                    else
+                    {
+                        sqlString += " AND ";
+                    }
+                    sqlString += "date BETWEEN @dateFrom AND @dateTo";
+                    command.Parameters.AddWithValue("@dateFrom", dateFrom);
+                    command.Parameters.AddWithValue("@dateTo", dateTo);
+
+                }
+
+                command.CommandText = sqlString;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    VCardTransaction transaction = new VCardTransaction
+                    {
+                        Id = (int)reader["Id"],
+                        VCard = (string)reader["vcard"],
+                        Date = (DateTime)reader["date"],
+                        Type = char.Parse((string)reader["type"]),
+                        Value = reader.GetDecimal(4),
+                        Category_id = reader["category_id"] == DBNull.Value ? 0 : (int)reader["category_id"],
+                        Description = reader["description"] == DBNull.Value ? null : (string)reader["description"],
+                    };
+
+                    transactions.Add(transaction);
+                }
+
+                reader.Close();
+
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                return InternalServerError(e);
+            }
+
+            return Ok(transactions);
+        }
+
         [Route("api/vcards/{phone_number}/transactions")]
         public IHttpActionResult GetTransactionsOfVcard(string phone_number)
         {
@@ -62,7 +139,7 @@ namespace BankOne.Controllers
 
             return Ok(transactions);
         }
-        
+
         [Route("api/users/{id:int}/transactions")]
         public IHttpActionResult GetTransactionsOfUser(int id)
         {
@@ -118,13 +195,13 @@ namespace BankOne.Controllers
 
             if (transaction.Value <= 0)
                 return Content((HttpStatusCode)422, "Invalid transaction value");
-            
+
             if (transaction.Type != 'D' && transaction.Type != 'C')
                 return Content((HttpStatusCode)422, "Invalid type of transaction");
-            
+
             if (transaction.Description != null && transaction.Description.Length > 255)
                 return Content((HttpStatusCode)422, "Invalid description (Must be smaller than 255 characters");
-            
+
             if (transaction.Category_id < 0)
             {
                 return Content((HttpStatusCode)422, "Invalid category");
@@ -136,9 +213,9 @@ namespace BankOne.Controllers
             try
             {
                 connection = new SqlConnection(connectionString);
-                
+
                 connection.Open();
-                
+
                 SqlCommand commandSearchVCard = new SqlCommand("SELECT * FROM VCards WHERE phone_number = @phone_number", connection);
                 commandSearchVCard.Parameters.AddWithValue("@phone_number", transaction.VCard);
                 SqlDataReader readerVCard = commandSearchVCard.ExecuteReader();
@@ -149,8 +226,8 @@ namespace BankOne.Controllers
                     connection.Close();
                     return Content((HttpStatusCode)422, "VCard doesn't exist");
                 }
-                
-                decimal earning_percentage = readerVCard.GetDecimal(3) > 0 ? readerVCard.GetDecimal(3)/100 : 0;
+
+                decimal earning_percentage = readerVCard.GetDecimal(3) > 0 ? readerVCard.GetDecimal(3) / 100 : 0;
                 decimal balance = readerVCard.GetDecimal(1);
                 decimal max_debit = readerVCard.GetDecimal(2);
                 int user_id = (int)readerVCard["user_id"];
@@ -173,19 +250,21 @@ namespace BankOne.Controllers
                     hashedReceivedConfirmationCode = Convert.ToBase64String(mySHA256.ComputeHash(Encoding.UTF8.GetBytes(confirmation_code)));
                 }
 
-                if (hashedReceivedConfirmationCode != (string)readerUser["confirmation_code"]){
+                if (hashedReceivedConfirmationCode != (string)readerUser["confirmation_code"])
+                {
                     readerUser.Close();
                     connection.Close();
                     return Content((HttpStatusCode)422, "Invalid confirmation code");
                 }
-                
+
                 readerUser.Close();
-                
+
                 if (balance < transaction.Value)
                 {
                     connection.Close();
                     return Content((HttpStatusCode)422, "Insufficient funds");
-                } else if (max_debit < transaction.Value)
+                }
+                else if (max_debit < transaction.Value)
                 {
                     connection.Close();
                     return Content((HttpStatusCode)422, "Value greater than the max debit of the selected vcard");
@@ -208,9 +287,9 @@ namespace BankOne.Controllers
 
                     readerCategory.Close();
                 }
-                
+
                 sqlTransaction = connection.BeginTransaction();
-                
+
                 string sql = "INSERT INTO VCardTransactions VALUES (@vcard, @date, @type, @value, @category_id, @description)";
                 SqlCommand commandInsertTransaction = new SqlCommand(sql, connection, sqlTransaction);
 
@@ -218,11 +297,12 @@ namespace BankOne.Controllers
                 commandInsertTransaction.Parameters.AddWithValue("@date", DateTime.Now);
                 commandInsertTransaction.Parameters.AddWithValue("@type", transaction.Type);
                 commandInsertTransaction.Parameters.AddWithValue("@value", transaction.Value);
-                
+
                 if (transaction.Category_id != 0)
                 {
                     commandInsertTransaction.Parameters.AddWithValue("@category_id", transaction.Category_id);
-                } else
+                }
+                else
                 {
                     commandInsertTransaction.Parameters.AddWithValue("@category_id", DBNull.Value);
                 }
@@ -230,7 +310,8 @@ namespace BankOne.Controllers
                 if (transaction.Description == null)
                 {
                     commandInsertTransaction.Parameters.AddWithValue("@description", DBNull.Value);
-                } else
+                }
+                else
                 {
                     commandInsertTransaction.Parameters.AddWithValue("@description", transaction.Description);
                 }
@@ -242,7 +323,7 @@ namespace BankOne.Controllers
                 if (transaction.Type == 'D')
                 {
                     operation = "-";
-                    transaction.Value -= transaction.Value*earning_percentage;
+                    transaction.Value -= transaction.Value * earning_percentage;
                 }
 
                 sql = $"UPDATE VCards SET balance = balance {operation} @value WHERE phone_number = @phone_number";
@@ -253,7 +334,7 @@ namespace BankOne.Controllers
                 commandUpdateBalance.Parameters.AddWithValue("@phone_number", transaction.VCard);
 
                 int numVCardsUpdated = commandUpdateBalance.ExecuteNonQuery();
-                
+
                 sqlTransaction.Commit();
 
                 sqlTransaction.Dispose();
@@ -275,7 +356,7 @@ namespace BankOne.Controllers
                     sqlTransaction.Rollback();
                     sqlTransaction.Dispose();
                 }
-                
+
                 if (connection.State == System.Data.ConnectionState.Open)
                 {
                     connection.Close();
@@ -291,7 +372,7 @@ namespace BankOne.Controllers
             {
                 return BadRequest();
             }
-            
+
             if (transactionDetails.Category_id < 0 && transactionDetails.GetIsChanged())
             {
                 return Content((HttpStatusCode)422, "Invalid category");
@@ -319,7 +400,7 @@ namespace BankOne.Controllers
                     connection.Close();
                     return NotFound();
                 }
-                
+
                 int old_category = readerTransaction["category_id"] == DBNull.Value ? 0 : (int)readerTransaction["category_id"];
                 string description = transactionDetails.Description ?? (string)readerTransaction["description"];
 
@@ -333,7 +414,7 @@ namespace BankOne.Controllers
                     readerVCard.Close();
                     throw new Exception();
                 }
-                
+
                 int user_id = (int)readerVCard["user_id"];
 
                 SqlCommand commandSearchUser = new SqlCommand("SELECT * FROM Users WHERE id = @id", connection);
@@ -353,12 +434,13 @@ namespace BankOne.Controllers
                     hashedReceivedConfirmationCode = Convert.ToBase64String(mySHA256.ComputeHash(Encoding.UTF8.GetBytes(transactionDetails.Confirmation_code)));
                 }
 
-                if (hashedReceivedConfirmationCode != (string)readerUser["confirmation_code"]){
+                if (hashedReceivedConfirmationCode != (string)readerUser["confirmation_code"])
+                {
                     readerUser.Close();
                     connection.Close();
                     return Content((HttpStatusCode)422, "Invalid confirmation code");
                 }
-                
+
                 readerUser.Close();
 
                 if (transactionDetails.Category_id > 0)
@@ -379,19 +461,22 @@ namespace BankOne.Controllers
                 }
 
                 command.Parameters.AddWithValue("@id", id);
-                
+
                 if (transactionDetails.Category_id > 0)
                 {
                     command.Parameters.AddWithValue("@category_id", transactionDetails.Category_id);
-                } else if (transactionDetails.Category_id == 0)
+                }
+                else if (transactionDetails.Category_id == 0)
                 {
                     command.Parameters.AddWithValue("@category_id", DBNull.Value);
-                } else 
+                }
+                else
                 {
                     if (old_category == 0)
                     {
                         command.Parameters.AddWithValue("@category_id", DBNull.Value);
-                    } else
+                    }
+                    else
                     {
                         command.Parameters.AddWithValue("@category_id", old_category);
                     }
