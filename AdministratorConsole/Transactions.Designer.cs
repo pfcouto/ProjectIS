ce1bd4854bd3d2913b2356bad53903b1398ae9de
Pedro Couto <2191265@my.ipleiria.pt>
2021-12-10 00:40:12 +0000

    Transactions Design, dont know why it didnt commit on last commit

---
+++ AdministratorConsole/Transactions.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace AdministratorConsole
+{
+    partial class Transactions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxExternalEntity = new System.Windows.Forms.ComboBox();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerOrigin = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
+            this.dataGridViewTransactions = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VCardOrigin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VCardDestiny = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonExportExcel = new System.Windows.Forms.Button();
+            this.buttonExportXML = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransactions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxExternalEntity
+            // 
+            this.comboBoxExternalEntity.FormattingEnabled = true;
+            this.comboBoxExternalEntity.Location = new System.Drawing.Point(54, 40);
+            this.comboBoxExternalEntity.Name = "comboBoxExternalEntity";
+            this.comboBoxExternalEntity.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxExternalEntity.TabIndex = 0;
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Location = new System.Drawing.Point(237, 40);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxType.TabIndex = 1;
+            // 
+            // dateTimePickerOrigin
+            // 
+            this.dateTimePickerOrigin.Location = new System.Drawing.Point(447, 40);
+            this.dateTimePickerOrigin.Name = "dateTimePickerOrigin";
+            this.dateTimePickerOrigin.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerOrigin.TabIndex = 2;
+            this.dateTimePickerOrigin.Value = new System.DateTime(2021, 12, 9, 17, 38, 19, 0);
+            // 
+            // dateTimePickerTo
+            // 
+            this.dateTimePickerTo.Location = new System.Drawing.Point(685, 40);
+            this.dateTimePickerTo.Name = "dateTimePickerTo";
+            this.dateTimePickerTo.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerTo.TabIndex = 3;
+            // 
+            // dataGridViewTransactions
+            // 
+            this.dataGridViewTransactions.AllowUserToAddRows = false;
+            this.dataGridViewTransactions.AllowUserToDeleteRows = false;
+            this.dataGridViewTransactions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewTransactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTransactions.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Type,
+            this.VCardOrigin,
+            this.VCardDestiny,
+            this.Date,
+            this.Value});
+            this.dataGridViewTransactions.Location = new System.Drawing.Point(54, 87);
+            this.dataGridViewTransactions.Name = "dataGridViewTransactions";
+            this.dataGridViewTransactions.ReadOnly = true;
+            this.dataGridViewTransactions.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewTransactions.Size = new System.Drawing.Size(831, 318);
+            this.dataGridViewTransactions.TabIndex = 4;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            // 
+            // VCardOrigin
+            // 
+            this.VCardOrigin.HeaderText = "VCard Origin";
+            this.VCardOrigin.Name = "VCardOrigin";
+            this.VCardOrigin.ReadOnly = true;
+            // 
+            // VCardDestiny
+            // 
+            this.VCardDestiny.HeaderText = "VCard Destiny";
+            this.VCardDestiny.Name = "VCardDestiny";
+            this.VCardDestiny.ReadOnly = true;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "External Entity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(444, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "From";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(682, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "To";
+            // 
+            // buttonExportExcel
+            // 
+            this.buttonExportExcel.Location = new System.Drawing.Point(608, 431);
+            this.buttonExportExcel.Name = "buttonExportExcel";
+            this.buttonExportExcel.Size = new System.Drawing.Size(116, 39);
+            this.buttonExportExcel.TabIndex = 9;
+            this.buttonExportExcel.Text = "Export to Excel";
+            this.buttonExportExcel.UseVisualStyleBackColor = true;
+            // 
+            // buttonExportXML
+            // 
+            this.buttonExportXML.Location = new System.Drawing.Point(769, 431);
+            this.buttonExportXML.Name = "buttonExportXML";
+            this.buttonExportXML.Size = new System.Drawing.Size(116, 39);
+            this.buttonExportXML.TabIndex = 10;
+            this.buttonExportXML.Text = "Export to XML";
+            this.buttonExportXML.UseVisualStyleBackColor = true;
+            // 
+            // Transactions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 497);
+            this.Controls.Add(this.buttonExportXML);
+            this.Controls.Add(this.buttonExportExcel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewTransactions);
+            this.Controls.Add(this.dateTimePickerTo);
+            this.Controls.Add(this.dateTimePickerOrigin);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.comboBoxExternalEntity);
+            this.Name = "Transactions";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transactions";
+            this.Load += new System.EventHandler(this.Transactions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransactions)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxExternalEntity;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.DateTimePicker dateTimePickerOrigin;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTo;
+        private System.Windows.Forms.DataGridView dataGridViewTransactions;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VCardOrigin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VCardDestiny;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonExportExcel;
+        private System.Windows.Forms.Button buttonExportXML;
+    }
+}
---
+++ AdministratorConsole/Transactions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdministratorConsole
+{
+    public partial class Transactions : Form
+    {
+        public Transactions()
+        {
+            InitializeComponent();
+        }
+
+        private void Transactions_Load(object sender, EventArgs e)
+        {
+            dateTimePickerOrigin.Value = DateTime.Today;
+            dateTimePickerTo.Value = DateTime.Now;
+
+        }
+    }
+}
